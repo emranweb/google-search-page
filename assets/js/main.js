1170987db074bcd63d9b47564ed2f480f8134a79
emran <emranbd77@gmail.com>
2017-03-20 13:03:07 +0600

    Tools area compleate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,32 @@
 $(function () {
+    $('html').click(function () {
+        $('.tools').fadeOut(300);
+        $('.notification').fadeOut(300);
+        $('.profile').fadeOut(100);
+    });
+    $('.icon').click(function (event) {
+        event.stopPropagation();
+    });
+
     $('.icon i:first').click(function () {
-        $('.tools').show().css('display', 'flex');
-        });
+        $('.tools').fadeIn(500).css('display', 'flex');
+    });
+    $('.icon i:first').siblings().click(function () {
+        $('.tools').fadeOut(100);
+    });
+
+    $('.icon i:nth-child(2)').click(function () {
+        $('.notification').fadeIn(500).css('display', 'flex');
+    });
+    $('.icon i:nth-child(2)').siblings().click(function () {
+        $('.notification').fadeOut(100);
+    });
+
+    $('.icon span').click(function () {
+        $('.profile').fadeIn(500);
+    })
+    $('.icon span').siblings().click(function () {
+        $('.profile').fadeOut(100);
+    })
 
 })
